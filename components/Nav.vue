7d1d5b9b73dfcb3a32bc348cba513f6583d0c29d
Dawn <dtrail76@gmail.com>
2024-09-14 11:46:18 +0100

    update nav to correctly use badge

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,26 +1,36 @@
 <script setup>
-import { useTodoStore } from '~/stores/todoStore';
-import { storeToRefs } from 'pinia';
+import { useTodoStore } from "~/stores/todoStore";
+import { storeToRefs } from "pinia";
 
 const store = useTodoStore();
-const {todos} = storeToRefs(store)
+const { todos } = storeToRefs(store);
 
 const badgeCount = computed(() => todos.value.length);
 
-const links = [ {
-  label: "todos",
-  icon: "i-heroicons-home",
-  to: "/",
-//   badge: badgeCount
+const links = [
+  {
+    label: "todos",
+    icon: "i-heroicons-home",
+    to: "/",
   },
-{
-  label: "Completed",
-  icon: "i-heroicons-check",
-  to: "/completed"
-},
-]
+  {
+    label: "Completed",
+    icon: "i-heroicons-check",
+
+    to: "/completed",
+  },
+];
 </script>
 
 <template>
-    <UHorizontalNavigation :links="links" class="border-b border-gray-200 dark:border-gray-800" />
+  <nav class="border-b border-gray-200 dark:border-gray-800">
+    <NuxtLink to="/"> 
+        <UBadge :label="badgeCount" />
+        Todos
+         </NuxtLink>
+    <NuxtLink to="/completed">
+      <UIcon name="i-heroicons-check" class="w-5 h-5" />
+      completed
+    </NuxtLink>
+  </nav>
 </template>
