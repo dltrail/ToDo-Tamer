2337eeec1b84f942494c5d2d018fda4dc7d0ade7
Dawn <dtrail76@gmail.com>
2024-09-15 15:46:59 +0100

    Move footer to a new component

---
+++ components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="footer">
+    <p>
+      Designed and Dev'ed by
+      <NuxtLink class="footerLink" to="https://github.com/dltrail"
+        >D Trail</NuxtLink
+      >
+    </p>
+  </footer>
+</template>
+
+<style>
+.footer {
+  @apply absolute bottom-0 flex justify-center w-screen;
+}
+
+.footerLink {
+  @apply font-bold hover:underline underline-offset-2;
+}
+</style>
--- components/Layout.vue
+++ components/Layout.vue
@@ -1,25 +1,11 @@
 <template>
   <main>
-    <div class="todoApp">
-        ToDo App
-    </div> 
-    <TodoForm/>
+    <div class="text-customGreen">ToDo App</div>
+    <TodoForm />
     <Nav />
     <slot />
-    <footer class="footer">
-      <p>Designed and deved by <NuxtLink class="footerLink" to="https://github.com/dltrail">dtrail</NuxtLink></p>
-    </footer>
+    <Footer />
   </main>
 </template>
-
-<style>
-.footer {
-  @apply absolute bottom-0 flex justify-center w-screen
-}
-
-.footerLink {
-  @apply font-bold hover:underline underline-offset-2
-}
-</style>
 
 
