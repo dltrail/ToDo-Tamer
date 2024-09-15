0bf9532de9de7978dde174536d6659b7b1b48c93
Dawn <dtrail76@gmail.com>
2024-09-15 12:23:21 +0100

    filter completed

--- components/TodoList.vue
+++ components/TodoList.vue
@@ -4,16 +4,15 @@ import { storeToRefs } from 'pinia';
 
 const store = useTodoStore();
 const {todos} = storeToRefs(store)
+const completedTodos = computed(() => store.todos.filter(todo => todo.completed !== true))
 
 </script>
 
 <template>
         <div class="todoList">
-         
-        
-           <div v-if="todos.length > 0" v-for="todo in todos" :key="todo.id" class="item" >
-          
-            <Todo v-if="!todo.completed" :todo="todo"/>
+           <div v-if="todos.length > 0" v-for="todo in completedTodos" :key="todo.id" class="item" >
+
+            <Todo :todo="todo"/>
            </div>
 
              <div v-else>
