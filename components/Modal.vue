1cf4bd9e8eeade08f7a05b623521ee9c38f4be83
Dawn <dtrail76@gmail.com>
2024-09-15 13:19:08 +0100

    Add modal functionality

---
+++ components/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+  <UModal
+    class="modal"
+    :ui="{
+      strategy: 'override',
+      container: 'flex min-h-full items-center justify-center text-center',
+      background: 'bg-red-200',
+      text: 'bg-red-20',
+    }"
+  >
+    <div class="modalInner">
+      <p>Are you sure you want to clear all todo's</p>
+      <UButtonGroup size="sm" orientation="horizontal">
+        <UButton variant="outline" @click="$emit('toggleModal')"
+          >Cancel</UButton
+        >
+        <UButton @click="$emit('acceptModal')" variant="solid"
+          >I'm ready</UButton
+        >
+      </UButtonGroup>
+    </div>
+  </UModal>
+</template>
--- pages/completed.vue
+++ pages/completed.vue
@@ -1,14 +1,44 @@
 <script setup>
 useHead({
-  title: 'My Completed Todos',
-  meta: [
-    { name: 'description', content: 'completed todos' }
-  ]
-})
+  title: "My Completed Todos",
+  meta: [{ name: "description", content: "completed todos" }],
+});
+
+import { useTodoStore } from "~/stores/todoStore";
+const store = useTodoStore();
+const {todos} = storeToRefs(store)
+const isOpen = ref(false);
+const completed = computed(() => store.todos.filter(todo => todo.completed))
+
+
+// Handle modal open and close
+const handleToggleModalVisibility = () => {
+  isOpen.value = !isOpen.value
+};
+
+const handleConfirmBtnClick = () => {
+  store.clearCompletedTodos()
+  handleToggleModalVisibility()
+
+}
+
 </script>
 
 <template>
-    <Layout>
-        <CompletedTodos/>
-    </Layout>
+  <Layout>
+    <UButton @click="handleToggleModalVisibility" :disabled="completed.length === 0">Clear All</UButton>
+
+    <CompletedTodos/>
+    <Modal v-model="isOpen" class="modal" @toggleModal="handleToggleModalVisibility" @acceptModal="handleConfirmBtnClick" />
+  </Layout>
 </template>
+
+<style>
+.modal {
+  @apply w-96 items-center;
+}
+
+.modalInner {
+  @apply p-4  text-gray-50 h-96;
+}
+</style>
