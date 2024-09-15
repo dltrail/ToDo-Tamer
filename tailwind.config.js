724339ceac66cc041866571bb48f52637d85514c
Dawn <dtrail76@gmail.com>
2024-09-15 15:47:31 +0100

    Install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+
+    },
+  },
+  plugins: [],
+}
+
