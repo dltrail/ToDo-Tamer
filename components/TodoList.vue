70eee1deabfcc6fb85ed1f0fa5c8fcbb62c243d9
Dawn <dtrail76@gmail.com>
2024-09-15 13:18:55 +0100

    formatting

--- components/TodoList.vue
+++ components/TodoList.vue
@@ -1,26 +1,29 @@
 <script setup>
-import { useTodoStore } from '~/stores/todoStore';
-import { storeToRefs } from 'pinia';
+import { useTodoStore } from "~/stores/todoStore";
+import { storeToRefs } from "pinia";
 
 const store = useTodoStore();
-const {todos} = storeToRefs(store)
-const completedTodos = computed(() => store.todos.filter(todo => todo.completed !== true))
-
+const { todos } = storeToRefs(store);
+const completedTodos = computed(() =>
+  store.todos.filter((todo) => todo.completed !== true)
+);
 </script>
 
 <template>
-        <div class="todoList">
-           <div v-if="todos.length > 0" v-for="todo in completedTodos" :key="todo.id" class="item" >
+  <div class="todoList">
+    <div
+      v-if="todos.length > 0"
+      v-for="todo in completedTodos"
+      :key="todo.id"
+      class="item"
+    >
+      <Todo :todo="todo" />
+    </div>
 
-            <Todo :todo="todo"/>
-           </div>
-
-             <div v-else>
+    <div v-else>
       <p>No todos available.</p>
     </div>
-        </div>
-  </template>
+  </div>
+</template>
 
-<style>
-
-</style>
+<style></style>
