ba6781b997551c51d1d3d2f39a6a1b73ed9e781d
Dawn <dtrail76@gmail.com>
2024-09-18 10:32:39 +0100

    Update empty list component

--- components/EmptyList.vue
+++ components/EmptyList.vue
@@ -1,12 +1,12 @@
 <template>
-   <div class="msg">
+   <div class="message">
    <slot/>
    </div>
 </template>
   
 
 <style scoped>
-.msg {
+.message {
     @apply text-center my-20 text-[var(--secondary-text-color)];
   }
 </style>
